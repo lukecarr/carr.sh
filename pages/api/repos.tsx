961b5fcc03c8ba0faa3163a0b9e81ad2ea89ccc7
Luke Carr <luke_jcarr@icloud.com>
2022-02-10 15:13:49 +0000

    ♻️ Moved repo fetching to API route (with caching)

---
+++ pages/api/repos.tsx
@@ -0,0 +1,26 @@
+import { Octokit } from '@octokit/rest'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const getRepos = async () => {
+  const github = new Octokit()
+  const { data } = await github.repos.listForUser({
+    username: 'lukecarr',
+    sort: 'pushed',
+  })
+
+  return {
+    repos: data.filter(
+      (repo) => !repo.archived && !['lukecarr'].includes(repo.name)
+    )
+  }
+}
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const repos = await getRepos()
+
+  if (process.env.NODE_ENV === 'production') res.setHeader('Cache-Control', `public,max-age=${60 * 60},immutable`)
+
+  res.status(200)
+    .setHeader('Content-Type', 'text/csv')
+    .send(JSON.stringify(repos))
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,34 @@
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { Octokit } from '@octokit/rest'
+import useSWR from 'swr'
 
 import Layout from '../components/layout'
 import Hero from '../components/hero'
 import Repos from '../components/repos'
 import Careers from '../components/careers'
 import Certifications from '../components/certifications'
-
-import { shields } from '../data/shields.json'
-import { education } from '../data/education.json'
-import { careers } from '../data/careers.json'
-import { certifications } from '../data/certifications.json'
 import Education from '../components/education'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const github = new Octokit()
-  const { data } = await github.repos.listForUser({
-    username: 'lukecarr',
-  })
+import shields from '../data/shields.json'
+import education from '../data/education.json'
+import careers from '../data/careers.json'
+import certifications from '../data/certifications.json'
 
-  return {
-    props: {
-      repos: data.filter(
-        (repo) => !repo.archived && !['lukecarr'].includes(repo.name)
-      ),
-    }
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+const Homepage = () => {
+  const { data, error } = useSWR('/api/repos', fetcher)
+
+  if (error) {
+    return (
+      <Layout>
+        <Head>
+          <title>Error</title>
+        </Head>
+        <p>Failed to fetch repositories!</p>
+      </Layout>
+    )
   }
-}
 
-const Homepage = ({ repos }) => {
   return (
     <Layout>
       <Head>
@@ -39,7 +38,7 @@ const Homepage = ({ repos }) => {
       <Careers careers={careers} />
       <Certifications certifications={certifications} />
       <Education education={education} />
-      <Repos repos={repos} />
+      <Repos repos={data?.repos ?? []} />
     </Layout>
   )
 }
