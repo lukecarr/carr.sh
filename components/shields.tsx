f53754d5548b6a0523d3d3e92fbd8771ab309a99
Luke Carr <luke_jcarr@icloud.com>
2022-03-24 15:46:39 +0000

    💄 Added skeleton loader for loading state

--- components/shields.tsx
+++ components/shields.tsx
@@ -1,3 +1,4 @@
+import ContentLoader from 'react-content-loader'
 import useSWR from 'swr'
 
 import type { FunctionComponent } from 'react'
@@ -13,11 +14,27 @@ type Shield = {
   color: string
 }
 
+const Loading: FunctionComponent = () => (
+  <ContentLoader
+    className="mt-8"
+    speed={2}
+    width={400}
+    height={24}
+    viewBox="0 0 400 24"
+    backgroundColor="#5c677d"
+    foregroundColor="#fefefe"
+  >
+    <rect x="0" y="0" rx="4" ry="4" width="120" height="24" /> 
+    <rect x="140" y="0" rx="4" ry="4" width="120" height="24" /> 
+    <rect x="280" y="0" rx="4" ry="4" width="120" height="24" />
+  </ContentLoader>
+)
+
 const Shields: FunctionComponent = () => {
   const { data, error } = useSWR<Shield[]>('/api/shields', fetcher)
 
   if (error) return <p>Failed to load shields!</p>
-  if (!data) return <p>Loading...</p>
+  if (!data) return <Loading />
 
   return (
     <div className="shields mt-4 text-center md:text-left space-y-4 space-x-4">
